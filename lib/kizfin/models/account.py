f9ffc5328a9af9860283faffbf17ba562e0b4be6
Markizano Draconus <markizano@markizano.net>
2022-09-11 01:47:44 -0500

    Have a complete thought on Transaction(). Building TransactionCollection(). Still building out Account() as well.

--- lib/kizfin/models/account.py
+++ lib/kizfin/models/account.py
@@ -1,13 +1,49 @@
 
 import kizfin.adapters.mongo
 
-class AccountModel(object):
+import kizfin.models.transaction
 
-    def __init__(self, cfg={}):
-        self.accountId          = cfg.accountId
-        self.routing            = cfg.routing
-        self.name               = cfg.name
-        self.institution        = cfg.institution
-        self.interestRate       = cfg.interestRate
-        self.interestStrategy   = cfg.interestStrategy
-        self.accountType        = cfg.accountType
+class Account(object):
+    '''
+    Account Object.
+    Represents an account object. Contains transactions and metadata about the account information.
+    '''
+
+    def __init__(self, accountId, **kwargs):
+        self.accountId          = accountId
+        self.routing            = kwargs.get('routing', None)
+        self.name               = kwargs.get('name', None)
+        self.institution        = kwargs.get('institution', None)
+        self.interestRate       = kwargs.get('interestRate', None)
+        self.interestStrategy   = kwargs.get('interestStrategy', None)
+        self.accountType        = kwargs.get('accountType', None)
+        if 'transactions' in kwargs:
+            self.transactions   = TransactionCollection(kwargs['transactions'])
+
+    def __str__(self):
+        result = {
+            'accountId': self.accountId,
+        }
+        for prop in list( self.__dict__.keys() ):
+            if prop == 'accountId':
+                continue
+            value = getattr(self, prop)
+            if value is None:
+                continue
+            result[prop] = value
+        return json.dumps(result, default=str)
+
+    @property
+    def interestRate(self, value):
+        return self._interestRate
+    @interestRate.setter
+    def interestRate(self, value):
+        if value:
+            self._interestRate = float(value)
+    @property
+    def transaction(self):
+        return self._transaction
+    @transaction.setter
+    def transaction(self, value):
+        if not isinstance(value, kizfin.models.transaction.Transaction):
+            value = kizfin.models.transaction.Transaction(*value)
--- lib/kizfin/models/transaction.py
+++ lib/kizfin/models/transaction.py
@@ -1,23 +1,64 @@
 
+import re
+import json
+import dateparser
+
+class TransactionCollection(list):
+    '''
+    List of transactions we would like to contain, aggregate and summarize for the account.
+    '''
+    def __init__(self, items):
+        for item in items:
+            self.append(item)
+        return super().__init__(self)
+
+    def append(self, item):
+        if not isinstance(item, Transaction):
+            item = Transaction(*item)
+        return super().append(item)
+
 class Transaction(object):
     '''
     Transaction Object.
     Represents any transaction object.
     Null values are not carried over to __str__() representations.
     '''
-    def __init__(self, txnId, fromAccountId, toAccountId=None, datePending=None, datePosted=None, summary=None, amount=None, tax=None, remarks=None):
+    def __init__(self, txnId, fromAccountId, **kwargs):
         self.txnId = txnId
         self.fromAccountId = fromAccountId
-        self.toAccountId = toAccountId
-        self.datePending = datePending
-        self.datePosted = datePosted
-        self.summary = summary
-        self.amount = amount
-        self.tax = tax
-        self.remarks = remarks
+        self.toAccountId = kwargs.get('toAccountId', None)
+        self.datePending = kwargs.get('datePending', None)
+        self.datePosted = kwargs.get('datePosted', None)
+        self.amount = kwargs.get('amount', None)
+        self.tax = kwargs.get('tax', None)
+        self.summary = kwargs.get('summary', None)
+        self.remarks = kwargs.get('remarks', None)
+        self.tags = kwargs.get('tags', None)
+
+    def __str__(self):
+        result = {
+            'txnId': self.txnId,
+            'fromAccountId': self.fromAccountId,
+        }
+        for prop in list( self.__dict__.keys() ):
+            if prop in ('txnId', 'fromAccountId'):
+                continue
+            if prop.startswith('_'):
+                prop = prop[1:]
+            value = getattr(self, prop)
+            if value is None:
+                continue
+            result[prop] = value
+        return json.dumps(result, default=str)
+
+    def __repr__(self):
+        '''
+        For debugging purposes.
+        '''
+        return str(self)
 
     @property
-    def datePending(self, value):
+    def datePending(self):
         return self._datePending
     @datePending.setter
     def datePending(self, value):
@@ -26,7 +67,7 @@ class Transaction(object):
         self._datePending = value
 
     @property
-    def datePosted(self, value):
+    def datePosted(self):
         return self._datePosted
     @datePosted.setter
     def datePosted(self, value):
@@ -35,29 +76,33 @@ class Transaction(object):
         self._datePosted = value
 
     @property
-    def amount(self, value):
+    def amount(self):
         return self._amount
     @amount.setter
     def amount(self, value):
-        self._amount = float(value)
+        if value:
+            self._amount = float(value)
 
     @property
-    def tax(self, value):
+    def tax(self):
         return self._tax
     @tax.setter
     def tax(self, value):
-        self._tax = float(value)
+        if value:
+            self._tax = float(value)
 
     @property
-    def summary(self, value):
+    def summary(self):
         return self._summary
     @summary.setter
     def summary(self, value):
-        self._summary = re.sub(r'(?:\s{2,})', ' ', value)
+        if value:
+            self._summary = re.sub(r'(?:\s{2,})', ' ', value)
 
     @property
-    def description(self, value):
+    def description(self):
         return self._description
     @description.setter
     def description(self, value):
-        self._description = re.sub(r'(?:\s{2,})', ' ', value)
+        if value:
+            self._description = re.sub(r'(?:\s{2,})', ' ', value)
