99bff7a8ffbc885ba6504dfe589c009037510d19
Markizano Draconus <markizano@markizano.net>
2023-06-20 03:22:01 -0500

    adding a new account form. to be populated.

---
+++ web/app/forms/account/account-form.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AccountFormComponent } from './account-form.component';
+
+describe('AccountFormComponent', () => {
+  let component: AccountFormComponent;
+  let fixture: ComponentFixture<AccountFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AccountFormComponent]
+    });
+    fixture = TestBed.createComponent(AccountFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ web/app/forms/account/account-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yaba-account-form',
+  templateUrl: './account-form.component.html',
+  styleUrls: ['./account-form.component.css']
+})
+export class AccountFormComponent {
+
+}
