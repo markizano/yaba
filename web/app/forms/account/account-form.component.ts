fc9f8736e89411371a476556ad9700f2c404950d
Markizano Draconus <markizano@markizano.net>
2023-06-22 21:41:31 -0500

    updates to institutions and account forms.

--- web/app/forms/account/account-form.component.ts
+++ web/app/forms/account/account-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, NO_ERRORS_SCHEMA, Output } from '@angular/core';
+import { AccountTypes, Accounts, IAccount } from 'app/lib/accounts';
+import { Institutions } from 'app/lib/institutions';
+import { FormMode } from 'app/lib/structures';
 
 @Component({
   selector: 'yaba-account-form',
   templateUrl: './account-form.component.html',
-  styleUrls: ['./account-form.component.css']
+  styleUrls: ['./account-form.component.css'],
+  imports: [ CommonModule ],
+  standalone: true,
+  schemas: [ NO_ERRORS_SCHEMA ],
 })
 export class AccountFormComponent {
+  @Input() public account?: IAccount;
+  @Output() public accounts?: Accounts;
+  visible?: boolean;
+  mode: FormMode = FormMode.Create;
+  protected institutions: Institutions = new Institutions();
+  protected AccountTypes = AccountTypes;
 
+  constructor() {
+    this.reset();
+    //@TODO: Read institutions from database.
+  }
+  public save() {
+    // Perform form validation to ensure fields are not empty.
+    // If they are, display a message to the user.
+  }
+
+  public cancel() {
+    this.close();
+    this.reset();
+  }
+
+  public close() {
+    this.visible = false;
+  }
+
+  public reset() {
+    this.mode = FormMode.Create;
+  }
 }
--- web/app/forms/institution/institution-form.component.ts
+++ web/app/forms/institution/institution-form.component.ts
@@ -1,9 +1,9 @@
-import { Component, Input, Output } from '@angular/core';
-import { IInstitution, Institutions, Institution } from 'app/lib/institutions';
-import { FormMode } from 'app/lib/structures';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, Input, Output, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MapTypes } from 'app/lib/institutions';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+import { FormMode } from 'app/lib/structures';
+import { IInstitution, Institutions, Institution, MapTypes } from 'app/lib/institutions';
 import { TransactionFields } from 'app/lib/transactions';
 
 @Component({
@@ -13,17 +13,37 @@ import { TransactionFields } from 'app/lib/transactions';
   imports: [ CommonModule ],
   standalone: true,
   schemas: [ NO_ERRORS_SCHEMA ],
+  exportAs: 'institutionForm',
 })
 export class InstitutionFormComponent {
   @Input() institutions: Institutions = new Institutions();
   @Output() institution: IInstitution = new Institution();
-  visible?: boolean;
-  mode: FormMode = FormMode.Create;
+  public visible?: boolean;
+  public mode: FormMode = FormMode.Create;
+  public errors: string[] = []; // List of array messages to render to end-user.
   public readonly MapTypes = MapTypes;
+  public forms: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    mappings: new FormArray([
+      new FormGroup({
+        fromField: new FormControl('', Validators.required),
+        toField: new FormControl(TransactionFields.UNKNOWN, Validators.required),
+        mapType: new FormControl(MapTypes.static, Validators.required),
+      }),
+    ]),
+  });
 
+  /**
+   * Handles the click event to save the institution to the database.
+   * Performs additional validation and checks to make sure all fields are entered accordingly.
+   * Renders errors for the user until validation passes, then send the information to the database.
+   */
   public save() {
-    // Perform form validation to ensure fields are not empty.
-    // If they are, display a message to the user.
+    this.institution.name = this.institution.name.trim();
+    if ( this.institution.name.length > 255 ) {
+      this.errors.push('Name must be less than 255 characters.');
+    }
     this.institutions.push(this.institution);
   }
 
