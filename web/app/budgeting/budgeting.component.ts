8e88e435596281ba8b323bb98d414003832e48e3
Markizano Draconus <markizano@markizano.net>
2024-03-25 12:01:48 -0500

    got budgeting back visible.

--- web/app/budgeting/budgeting.component.ts
+++ web/app/budgeting/budgeting.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Account, Accounts } from 'app/lib/accounts';
+import { BudgetStruct } from 'app/lib/structures';
+import { Transactions } from 'app/lib/transactions';
 
 @Component({
   selector: 'yaba-budgeting',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./budgeting.component.css'],
 })
 export class BudgetingComponent {
+  @Input() fromDate: Date;
+  @Input() toDate: Date;
+  @Input() selectedAccounts: Account[]|Accounts;
+  @Input() description: string|RegExp;
+  @Input() txns: Transactions;
+  @Input() budgets: BudgetStruct[] = [];
+  error = '';
+  @Output() fromDateChange = new EventEmitter<Date>();
+  @Output() toDateChange = new EventEmitter<Date>();
+  @Output() descriptionChange = new EventEmitter<string|RegExp>();
 
+  constructor() {
+    this.txns = new Transactions();
+    this.fromDate = new Date();
+    this.toDate = new Date();
+    this.selectedAccounts = [];
+    this.description = '';
+  }
 }
--- web/app/budgeting/budgeting.module.ts
+++ web/app/budgeting/budgeting.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BudgetingComponent } from './budgeting.component';
+import { FormsModule } from '@angular/forms';
 
+import { BudgetingComponent } from 'app/budgeting/budgeting.component';
+import { TransactionsModule } from 'app/transactions/transactions.module';
+import { ControlsModule } from 'app/controls/controls.module';
+import { YabaTableBudgetsComponent } from 'app/tables/budgets/budgets-table.component';
 
 @NgModule({
   declarations: [
     BudgetingComponent,
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ControlsModule,
+    TransactionsModule,
+    YabaTableBudgetsComponent,
   ],
   exports: [
     BudgetingComponent,
+    YabaTableBudgetsComponent,
   ]
 })
 export class BudgetingModule { }
