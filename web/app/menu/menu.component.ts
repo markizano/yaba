cfe7271aadbec06812329accab2e8331630e1a3e
Markizano Draconus <markizano@markizano.net>
2024-06-15 14:45:37 -0500

    got the list laid out for wider screens.

--- web/app/menu/menu.component.ts
+++ web/app/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { routeConfig } from 'app/routing';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
+import { Observable, fromEvent, map, startWith, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'yaba-menu',
@@ -18,4 +19,20 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class MenuComponent {
     public routes = routeConfig;
+    isScreenSmall$: Observable<boolean> = new Observable<boolean>();
+    isScreenSmall = false;
+
+    ngOnInit(): void {
+        // Checks if screen size is less than 768 pixels
+        const checkScreenSize = () => this.isScreenSmall = document.body.offsetWidth < 768;
+      
+        // Create observable from window resize event throttled so only fires every 500ms
+        const screenSizeChanged$ = fromEvent(window, 'resize').pipe(throttleTime(500), map(checkScreenSize));
+      
+        // Start off with the initial value use the isScreenSmall$ | async in the
+        // view to get both the original value and the new value after resize.
+        this.isScreenSmall$ = screenSizeChanged$.pipe(startWith(checkScreenSize()))
+        console.log('MenuComponent().ngOnInit().isScreeSmall: ', this.isScreenSmall$);
+      }
+
 }
--- web/app/yaba.component.ts
+++ web/app/yaba.component.ts
@@ -1,37 +1,9 @@
 /* Angular Requirements */
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-/* Yaba/App Requirements */
-import { Institutions } from 'app/lib/institutions';
-import { Accounts } from 'app/lib/accounts';
-import { Transactions } from 'app/lib/transactions';
-import { Settings } from 'app/lib/structures';
-
-/* Services */
-// import { SessionManagementService } from 'app/session.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './yaba.component.html',
 })
 export class YabaComponent {
-    title = 'yaba';
-    // @Input() institutions: Institutions;
-    // @Output() institutionsChange = new EventEmitter<Institutions>();
-
-    // @Input() accounts: Accounts;
-    // @Output() accountsChange = new EventEmitter<Accounts>();
-
-    // @Input() transactions: Transactions;
-    // @Output() transactionsChange = new EventEmitter<Transactions>();
-
-    // @Input() settings: Settings
-    // @Output() settingsChange = new EventEmitter<Settings>();
-
-    constructor(/*protected session: SessionManagementService*/) {
-        // this.institutions = Institutions.fromString(this.session.getItem('institutions', '{}') as string);
-        // this.accounts = Accounts.fromString(this.session.getItem('accounts', '{}') as string);
-        // this.transactions = Transactions.fromString(this.session.getItem('transactions', '{}') as string);
-        // this.settings = Settings.fromString(this.session.getItem('settings', '{}') as string);
-    }
 }
