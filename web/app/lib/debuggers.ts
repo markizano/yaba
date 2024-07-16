72487e954a821ca9ae912789fc5678d6f505e611
Markizano Draconus <markizano@markizano.net>
2024-07-15 21:48:00 -0500

    updating the debugging page to be able to generate transactions again.

---
+++ web/app/lib/debuggers.ts
@@ -0,0 +1,229 @@
+/* eslint @typescript-eslint/no-explicit-any: 0 */
+import { v4 } from 'uuid';
+
+import { TransactionType } from 'app/lib/types';
+import { CurrencyType } from 'app/lib/structures';
+import { Account, AccountTypes } from 'app/lib/accounts';
+import { Transaction, Transactions } from 'app/lib/transactions';
+
+import { Institution, InstitutionMapping, InstitutionMappings, MapTypes } from 'app/lib/institutions';
+
+/**
+ * RandomArray objectlet for generating random lists of constructor items.
+ */
+class RandomArray extends Array {
+
+    constructor(...args: any[]) { super(...args); }
+
+    random() {
+        return this[Math.floor(Math.random() * this.length)];
+    }
+}
+
+/**
+ * SeedList objectlet for generating random lists of constructor items.
+ */
+export class SeedList {
+    institutions = new RandomArray(
+        {name: 'JPMC', description: 'JP Morgan Chase'},
+        {name: 'BoA', description: 'Bank of America'},
+        {name: 'WF', description: 'Wells Fargo'},
+        {name: 'Synchrony', description: 'Synchrony Bank'},
+        {name: 'CapOne', description: 'Capital One'},
+        {name: 'Fidelity', description: 'Fidelity Checking'},
+        {name: 'AmEx', description: 'American Express'},
+    );
+    merchants = new RandomArray(
+        'Macys', 'Dillards', 'Banana Republic', 'Gap', 'Old Navy', 'JC Pennys',
+        'Nordstrom', 'Nordstrom Rack',
+        'Target', 'Walmart', 'Kroger', 'Tom Thumb', 'King Soopers', 'WinnCo Foods',
+        'Dollar Tree', 'Dollar General', '99 cent store',
+        'Ace Hardware', 'Toms Mechanics',
+        'Toyota', 'Honda', 'Chevy', 'RAM', 'Loves', 'Race Track', 'Raceway',
+        '7-Eleven', 'Valero', 'BP: Better Petroleum', 'Shell Gas', 'Robinhood'
+    );
+    products = new RandomArray(
+        '2% Milk', 'Whole Milk', 'Half and Half', 'Heavy Whipping Cream',
+        'Eggs', 'Cheese', 'Lunchmeat', 'Bread', 'Coffee', 'Ice Cream', 'Chips',
+        'Pickles', 'Olives', 'Oil change', 'Brake Check', 'Tire Rotation', 'Fleece Shirt',
+        'Barbie Doll', 'Ken Doll', `Action Figure #${SeedList.number(0, 10, 0)}`,
+        'Sausage', 'Bacon', 'Ham', 'Onion', 'Celery', 'Jeans',
+    );
+    payments = new RandomArray(
+        'Mortgage', 'Rent', 'Penalty', 'Credit Card Payment', 'Electric Bill', 'Groceries',
+        'Gas', 'Water Bill', 'Internet', 'Cable', 'Tax Return Payments',
+        'Auto Insurance', 'Home Insurance', 'Health Insurance',
+    );
+    investments = new RandomArray(
+        'Dividends', 'S&P 500', 'QQQ', 'Crypto', 'Real Estate', 'REIT', 'ETF',
+        'Bonds', 'T-Bills', 'Gold'
+    );
+    income = new RandomArray('Payroll', 'Income', 'Tips', 'Gifts', 'Sales', 'Dividends', 'Tax Returns');
+    transactionTypes = new RandomArray('withdraw', 'deposit');
+    headerTypes = {
+        datePending: new RandomArray('Requested Date', 'Date Pending', 'Pending Date', 'Pending'),
+        datePosted: new RandomArray('Transaction Date', 'Txn Date', 'Date Posted', 'Posted Date', 'Posted'),
+        amount: new RandomArray('Amount', 'amount', 'Cost', 'Debit', 'Credit'),
+        description: new RandomArray('Description', 'description', 'Memo', 'Remarks'),
+        transactionType: new RandomArray('TxnType', 'Type', 'Transaction Type'),
+    };
+    accountTypes = new RandomArray(...Object.values(AccountTypes));
+
+    static number(lower = 0, upper = 100, dec=2): number {
+        // Gimmie a random number no greater than ${upper} as float() with ${dec} number of decimal places.
+        const decimals = (value: string, index: number) => index? value.slice(0, dec): value;
+        return Number((Math.random() * (upper - lower) + lower).toString().split('.').map<string>(decimals).join('.'));
+    }
+
+    genInstitution(): Institution {
+        let name, description;
+        ((i) => { name = i.name; description = i.description })(this.institutions.random());
+        const mappings = new InstitutionMappings();
+        mappings.add(InstitutionMapping.fromObject({
+            fromField: this.headerTypes.datePending.random(),
+            toField: 'datePending',
+            mapType: MapTypes.dynamic
+        }));
+        mappings.add({
+            fromField: this.headerTypes.datePosted.random(),
+            toField: 'datePosted',
+            mapType: MapTypes.dynamic
+        });
+        mappings.add({
+            fromField: this.headerTypes.amount.random(),
+            toField: 'amount',
+            mapType: MapTypes.dynamic
+        });
+        mappings.add({
+            fromField: this.headerTypes.description.random(),
+            toField: 'description',
+            mapType: MapTypes.dynamic
+        });
+        mappings.add({
+            fromField: this.headerTypes.transactionType.random(),
+            toField: 'transactionType',
+            mapType: MapTypes.dynamic
+        });
+        mappings.add({
+            fromField: 'USD',
+            toField: 'currency',
+            mapType: MapTypes.dynamic
+        });
+        return Institution.fromObject({name, description, mappings});
+    }
+
+    genAccount(institutionId: string): Account {
+        let name, description;
+        ((i) => { name = i.name; description = i.description })(this.institutions.random());
+        return Account.fromObject({
+            institutionId: institutionId,
+            name: name,
+            description: description,
+            accountType: this.accountTypes.random(),
+            transactions: new Transactions()
+        });
+    }
+
+    genTransaction(accountId: string, o: any): Transaction {
+        o.maxAmount = o.maxAmount || 100;
+        o.minAmount = o.minAmount || 0;
+        o.since = o.since || 31536000000; // 1 year by default.
+        const ttype: TransactionType = o.ttype? o.ttype: this.transactionTypes.random(),
+          institution = this.institutions.random(),
+          amount = SeedList.number(o.minAmount, o.maxAmount, 2),
+          datePosted = new Date(Date.now() - (Math.floor(Math.random() * o.since * 1000))),
+          datePending = new Date(datePosted.getTime() - 259200000), // banks usually post in 3 business days.
+          result = new Transaction();
+        switch(ttype) {
+            case TransactionType.Credit:
+                switch(o.withdrawType) {
+                    case 'sale': {
+                        // in case of sale of item, like on credit card.
+                        const merchant = this.merchants.random();
+                        return Transaction.fromObject({
+                            id: v4(),
+                            description: `Purchased ${this.products.random()} from ${merchant}.`,
+                            accountId: accountId,
+                            datePending: datePending,
+                            datePosted: datePosted,
+                            transactionType: ttype,
+                            amount: -amount,
+                            tax: amount * 0.09,
+                            currency: CurrencyType.USD,
+                            merchant: merchant,
+                            tags: [],
+                        });
+                    }
+                    case 'payment': {
+                        // In case we are making like a credit card payment.
+                        // in case of sale of item, like on credit card.
+                        const billCollector = this.institutions.random().description;
+                        const bill = this.payments.random();
+                        return Transaction.fromObject({
+                            id: v4(),
+                            description: `Paid on ${bill} from ${billCollector}.`,
+                            accountId: accountId,
+                            datePending: datePending,
+                            datePosted: datePosted,
+                            transactionType: ttype,
+                            amount: -amount,
+                            tax: 0.0,
+                            currency: CurrencyType.USD,
+                            merchant: billCollector,
+                            tags: [],
+                        });
+                    }
+                }
+                break;
+            case TransactionType.Debit: {
+                const payroll = this.income.random();
+                return Transaction.fromObject({
+                    id: v4(),
+                    description: `Income ${payroll} from ${institution.description}`,
+                    accountId: accountId,
+                    datePending: datePending,
+                    datePosted: datePosted,
+                    transactionType: ttype,
+                    amount: amount,
+                    tax: 0,
+                    currency: CurrencyType.USD,
+                    merchant: payroll,
+                    tags: [],
+                });
+            }
+            case TransactionType.Payment: {
+                const merchant = this.merchants.random();
+                return Transaction.fromObject({
+                    id: v4(),
+                    description: `Transfer to ${merchant}`,
+                    accountId: accountId,
+                    datePending: datePending,
+                    datePosted: datePosted,
+                    transactionType: ttype,
+                    amount: -amount,
+                    tax: 0,
+                    currency: CurrencyType.USD,
+                    merchant: merchant,
+                    tags: [],
+                });
+            }
+            case TransactionType.Transfer: {
+                const investment = this.investments.random();
+                return Transaction.fromObject({
+                    id: v4(),
+                    description: `Investment in ${investment}`,
+                    accountId: accountId,
+                    datePending: datePending,
+                    datePosted: datePosted,
+                    transactionType: ttype,
+                    amount: -amount,
+                    tax: 0,
+                    currency: CurrencyType.USD,
+                    merchant: investment,
+                    tags: [],
+                });
+            }
+        }
+        return result;
+    }
+}
--- web/app/pages/develop/develop.component.ts
+++ web/app/pages/develop/develop.component.ts
@@ -1,16 +1,22 @@
-import { Component, Inject } from '@angular/core';
-import { AccountsFilterComponent } from 'app/controls/account-filter.component';
 
-import { EMPTY_TRANSACTION_FILTER } from 'app/lib/constants';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+import { Subscription } from 'rxjs';
+import * as saveAs from 'file-saver';
+
+import { EMPTY_TRANSACTION_FILTER, NULLDATE } from 'app/lib/constants';
+import { Settings } from 'app/lib/settings';
+import { SeedList } from 'app/lib/debuggers';
+import { Institution, InstitutionMapping, Institutions, MapTypes } from 'app/lib/institutions';
+import { Account, Accounts, AccountTypes } from 'app/lib/accounts';
 import { Transaction, Transactions } from 'app/lib/transactions';
+
+import { AccountsFilterComponent } from 'app/controls/account-filter.component';
+import { ControlsModule } from 'app/controls/controls.module';
 import { AccountsService } from 'app/services/accounts.service';
 import { TransactionsListComponent } from 'app/tables/transactions/transactions-list.component';
-import { NULLDATE } from 'app/lib/constants';
-import { Account, Accounts } from 'app/lib/accounts';
-import { ControlsModule } from 'app/controls/controls.module';
-import { Subscription } from 'rxjs';
-import { DOCUMENT } from '@angular/common';
-import { Institution, Institutions } from 'app/lib/institutions';
+
 
 @Component({
     selector: 'debugging',
@@ -26,6 +32,13 @@ export class DevelopComponent {
     transactions = new Transactions();
     filters = EMPTY_TRANSACTION_FILTER;
     #cachedUpdate?: Subscription;
+
+    // SeedList and CSV file generation testing
+    seedlist = new SeedList();
+    institution?: Institution;
+    account?: Account;
+    transaction?: Transaction;
+    csvFile = '';
 
     constructor(protected accountsService: AccountsService, @Inject(DOCUMENT) protected document: Document ) {
         this.filters.fromDate = NULLDATE;
@@ -54,5 +67,94 @@ export class DevelopComponent {
 
     log(data: unknown) {
         console.log(data);
+    }
+
+    produceCSV(txnCount: number = 10, acctType: string) {
+        const map2string = (t: Transaction) => (m: InstitutionMapping) => m.toField !== undefined && (t[m.toField] instanceof Date? (<Date>t[m.toField]).toISOShortDate(): t[m.toField]);
+        const institution = this.seedlist.genInstitution();
+        const account = this.seedlist.genAccount(institution.id);
+        const mapFields = institution.mappings.filter(m => m.mapType == MapTypes.dynamic);
+        const oneYear = new Date(Date.now() - Settings.fromLocalStorage().txnDelta).setDate(15),
+          today = new Date();
+        const nextDay = (dt: Date): number => {
+            const day = dt.getDate();
+            if ( day <= 10 ) return 15;
+            if ( day > 17 ) return 45;
+            if ( day < 30 ) return dt.getMonth() == 1? 28: 30;
+            if ( day >= 30 ) return 45;
+            return 1;
+        }
+        switch(acctType) {
+            case AccountTypes.Checking:
+                // l=0 is backup break from loop
+                for ( let start = new Date(oneYear), l=0; start <= today && l<=500; start.setDate(nextDay( start )), l++  ) {
+                    console.log('payday: ', start.toISOShortDate());
+                    account.transactions.add(this.seedlist.genTransaction(
+                        account.id, {
+                            ttype: 'deposit',
+                            maxAmount: 1200,
+                            minAmount: 950
+                        }
+                    ));
+                }
+                console.log('income txns: ', account.transactions.length);
+                for ( let i=0; i <= SeedList.number(account.transactions.length/2, account.transactions.length, 0); i++ ) {
+                    account.transactions.add(this.seedlist.genTransaction(
+                        account.id, {
+                            ttype: 'withdraw',
+                            withdrawType: 'payment',
+                            maxAmount: 300
+                        }
+                    ));
+                }
+                break;
+            case AccountTypes.Savings:
+                for ( let start = new Date(oneYear); start <= today; start.setDate( nextDay(start) )  ) {
+                    account.transactions.add(this.seedlist.genTransaction(
+                        account.id, {
+                            ttype: 'deposit',
+                            maxAmount: 120,
+                            minAmount: 90
+                        }
+                    ));
+                }
+                break;
+            case AccountTypes.Credit:
+                for ( let i=0; i <= txnCount; i++ ) {
+                    account.transactions.add(this.seedlist.genTransaction(account.id, {ttype: 'withdraw', withdrawType: 'sale'}));
+                }
+                break;
+        }
+        account.transactions.sorted();
+        this.csvFile = '"' + mapFields.map(m => m.fromField).join('","') + '"\n"'
+          + account.transactions
+            .map((t: Transaction) => mapFields.map(map2string(t))
+            .join('","') )
+            .join('"\n"') + '"';
+    }
+
+    downloadCSV() {
+        const csvBlob = new Blob([this.csvFile], { type: 'text/csv' } );
+        saveAs(csvBlob, 'mock-transactions.csv');
+    }
+
+    genInstitution() {
+        this.institution = this.seedlist.genInstitution();
+    }
+
+    genAccount() {
+        this.account = this.seedlist.genAccount(this.institution?.id || '');
+    }
+
+    genTransaction() {
+        this.transaction = this.seedlist.genTransaction(this.account?.id || '', {ttype: 'withdraw', withdrawType: 'sale'});
+    }
+
+    bindInstitution($event: string) {
+        (<Institution>this.institution).id = $event;
+    }
+
+    bindAccount($event: string) {
+        (<Account>this.account).id = $event;
     }
 }
