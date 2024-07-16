da7aa3176e48550750ed70916e3c521ec946bd16
Markizano Draconus <markizano@markizano.net>
2024-07-15 21:49:58 -0500

    new component that will allow for drop-down select of institutions.

---
+++ web/app/controls/institution-select.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Institutions } from "app/lib/institutions";
+import { InstitutionsService } from "app/services/institutions.service";
+import { Subscription } from "rxjs";
+
+/**
+ * NgSelect front for selecting institution(s).
+ */
+@Component({
+    selector: 'institution-select',
+    templateUrl: './institution-select.component.html',
+})
+export class InstitutionSelectComponent {
+    @Input() multiple = false;
+    @Output() selected = new EventEmitter<string[]|string>();
+    institutions = new Institutions();
+    #sub?: Subscription;
+    constructor(protected institutionsService: InstitutionsService) { }
+    ngOnInit() {
+        const update = (institutions: Institutions) => {
+            this.institutions = institutions;
+        }
+        update(this.institutionsService.get());
+        this.#sub = this.institutionsService.subscribe(update);
+    }
+    ngOnDestroy() {
+        this.#sub?.unsubscribe();
+    }
+}
