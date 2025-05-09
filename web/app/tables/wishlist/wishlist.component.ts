e0f4e74c850013d37cc69eedfe4e8f48efddc60d
Markizano Draconus <markizano@markizano.net>
2025-05-09 05:46:11 -0600

    dressing up the styles on the prospecting page.

--- web/app/tables/wishlist/wishlist.component.ts
+++ web/app/tables/wishlist/wishlist.component.ts
@@ -1,7 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ControlsModule } from 'app/controls/controls.module';
-import { Transactions } from 'app/lib/transactions';
+import { Transaction, Transactions } from 'app/lib/transactions';
 
+/**
+ * Wishlist Editing Component for widget
+ * Helps the user edit stuff for their wishlist on the prospecting page.
+ */
 @Component({
   selector: 'yaba-wishlist',
   standalone: true,
@@ -11,6 +15,37 @@ import { Transactions } from 'app/lib/transactions';
   templateUrl: './wishlist.component.html',
 })
 export class WishlistComponent {
-    @Input()
-    wishlist: Transactions = new Transactions();
+    @Input() wishlist: Transactions = new Transactions();
+    @Output() wishlistChange = new EventEmitter<Transactions>();
+
+    /* The item in question for editing. */
+    item: Transaction = new Transaction();
+
+    /* Are we editing this item or adding a new one? */
+    editing: boolean = false;
+
+    add() {
+        this.wishlist.add(this.item);
+        this.item = new Transaction();
+        this.editing = false;
+    }
+
+    edit(index: number) {
+        this.editing = true;
+        this.item = this.wishlist[index];
+    }
+
+    remove(id: string) {
+        this.wishlist.remove(id);
+    }
+
+    cancel() {
+        this.editing = false;
+        this.item = new Transaction();
+    }
+
+    save() {
+        this.item = new Transaction();
+        this.editing = false;
+    }
 }
