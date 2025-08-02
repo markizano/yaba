5179cb7d28b6a0632b0eecb5a78847803a3e6c3d
Markizano Draconus <markizano@markizano.net>
2025-08-02 11:33:00 -0500

    Revert "removing obsolete component."
    This reverts commit 55ff0fa75370613c7c151a9d074b31de5fc5d56f.

---
+++ web/app/transactions/transactions.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+
+import { ControlsModule } from 'app/controls/controls.module';
+import { TransactionsComponent } from 'app/transactions/transactions.component';
+import { TransactionsListComponent } from 'app/tables/transactions/transactions-list.component';
+
+@NgModule({
+  declarations: [
+    TransactionsComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatChipsModule,
+    MatIconModule,
+    FormsModule,
+    ControlsModule,
+    TransactionsListComponent,
+  ],
+  exports: [
+    TransactionsComponent,
+    TransactionsListComponent,
+  ]
+})
+export class TransactionsModule { }
---
+++ web/app/transactions/transactions.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yaba-transactions',
+  templateUrl: './transactions.component.html',
+  standalone: false,
+})
+export class TransactionsComponent {
+
+}
