1b32ed5b324a81ada023979db9ee93fe420f72b5
Markizano Draconus <markizano@markizano.net>
2025-08-10 10:28:55 -0500

    fix unused service implementation... probably will remove this later?

--- web/app/services/transactions.service.ts
+++ web/app/services/transactions.service.ts
@@ -1,5 +1,4 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { BaseHttpService } from 'app/services/basehttp.service';
 import { Transactions } from 'app/lib/transactions';
@@ -10,13 +9,11 @@ import { Transactions } from 'app/lib/transactions';
 export class TransactionsService extends BaseHttpService<Transactions> {
     readonly name = 'transactions';
     readonly endpoint = '/api/transactions';
-    protected cache: Transactions;
-    protected cacheSubject: EventEmitter<Transactions>;
+    protected cache: Transactions = new Transactions();
+    protected cacheSubject: EventEmitter<Transactions> = new EventEmitter<Transactions>();
 
-    constructor(http: HttpClient) {
-        super(http);
-        this.cache = new Transactions();
-        this.cacheSubject = new EventEmitter<Transactions>()
+    constructor() {
+        super();
         console.log('new TransactionsService()');
         this.load();
     }
