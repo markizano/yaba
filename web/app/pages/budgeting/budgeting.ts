996be4d3e30f036d7068c79b3fc9b1e260b2eb13
Markizano Draconus <markizano@markizano.net>
2025-08-10 22:29:20 -0500

    getting pages to work with updated versions of components.

--- web/app/pages/budgeting/budgeting.ts
+++ web/app/pages/budgeting/budgeting.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
 import { Budgets } from 'app/lib/types';
-import { EMPTY_TRANSACTION_FILTER } from 'app/lib/constants';
 import { Accounts } from 'app/lib/accounts';
 import { Transactions } from 'app/lib/transactions';
 import { AccountsService } from 'app/services/accounts.service';
@@ -12,34 +11,29 @@ import { Subscription } from 'rxjs';
  * The user can also edit the transactions on the page. Changing transactions influences the budgets panel.
  */
 @Component({
-    selector: 'yaba-budgeting',
-    templateUrl: './budgeting.html',
-    styleUrls: ['./budgeting.css'],
-    standalone: false,
+  selector: 'yaba-budgeting',
+  templateUrl: './budgeting.html',
+  styleUrls: ['./budgeting.css'],
+  standalone: false,
 })
 export class BudgetingComponent implements OnInit, OnDestroy {
-    txns = new Transactions();
-    budgets: Budgets = [];
-    #accts?: Subscription;
+  txns = new Transactions();
+  budgets: Budgets = [];
+  #accts?: Subscription;
 
-    accountsService = inject(AccountsService);
+  accountsService = inject(AccountsService);
 
-    ngOnInit() {
-        const update = (accounts: Accounts) => {
-            this.txns = Transactions.fromList(accounts.map(a => a.transactions).flat());
-            this.budgets = this.txns.getBudgets();
-            console.log('BudgetingComponent ngOnInit()', this.txns, this.budgets);
-        };
-        update(this.accountsService.get());
-        this.#accts = this.accountsService.subscribe(update);
-    }
+  ngOnInit() {
+    const update = (accounts: Accounts) => {
+      this.txns = Transactions.fromList(accounts.map(a => a.transactions).flat());
+      this.budgets = this.txns.getBudgets();
+      console.log('BudgetingComponent ngOnInit()', this.txns, this.budgets);
+    };
+    update(this.accountsService.get());
+    this.#accts = this.accountsService.subscribe(update);
+  }
 
-    ngOnDestroy() {
-        this.#accts?.unsubscribe();
-    }
-
-    localBudgets($event: Budgets) {
-        console.log('localBudgets()', $event);
-        this.budgets = $event;
-    }
+  ngOnDestroy() {
+    this.#accts?.unsubscribe();
+  }
 }
