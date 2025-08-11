7c46816c86197689e85665e41a5dba581389ddf9
Markizano Draconus <markizano@markizano.net>
2025-08-10 20:13:56 -0500

    removing txn-edit. too much of headache to try to get this to work.

--- web/app/tables/transactions/transaction-table/transaction-table.module.ts
+++ web/app/tables/transactions/transaction-table/transaction-table.module.ts
@@ -5,8 +5,7 @@ import { ControlsModule } from 'app/controls/controls.module';
 import { TxnRowComponent } from 'app/tables/transactions/transaction-table/txn-row/txn-row';
 import { TxnFieldComponent } from 'app/tables/transactions/transaction-table/txn-field/txn-field';
 import { TransactionTableComponent } from 'app/tables/transactions/transaction-table/transaction-table';
-import { TxnEditDirective } from 'app/tables/transactions/transaction-table/txn-edit/txn-edit.directive';
-import { TransactionSampleComponent } from '../transaction-sample/transaction-sample';
+import { TransactionSampleComponent } from 'app/tables/transactions/transaction-sample/transaction-sample';
 
 @NgModule({
   declarations: [
@@ -18,7 +17,6 @@ import { TransactionSampleComponent } from '../transaction-sample/transaction-sa
   imports: [
     ControlsModule,
     MatPaginator,
-    TxnEditDirective,
   ],
   exports: [
     TxnRowComponent,
--- web/app/tables/transactions/transaction-table/txn-edit/txn-edit.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TxnEditDirective } from './txn-edit.directive';
-
-describe('TxnEditDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TxnEditDirective();
-    expect(directive).toBeTruthy();
-  });
-});
